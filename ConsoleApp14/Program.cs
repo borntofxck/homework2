886e4d4fc7e1cf6170314af5e64bafeda04ade6c
kvtsuki <117033172+kvtsuki@users.noreply.github.com>
2023-09-20 23:41:28 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int newInt = 10;
+            double newFloat = 8.4;
+            string newStr = "True";
+
+            double bigInt = newInt * 3.5;
+
+            newFloat -= 1.0;
+
+            Console.WriteLine(newInt / newFloat);
+
+            Console.WriteLine(bigInt / newFloat);
+
+            newStr = "NoNo" + newStr + "YesYesYes";
+
+            Console.WriteLine("newInt: " + newInt);
+            Console.WriteLine("newFloat: " + newFloat);
+            Console.WriteLine("newStr: " + newStr);
+        }
+    }
+}
